bfd0de939a264dccaf90c303dcd1cd5113334ade
Michael Steinle <michael.steinle@gmail.com>
2021-12-02 10:24:59 +0100

    day 2 done

---
+++ adventofcode01/src/main/java/mist/adventofcode/day2/Main01.java
@@ -0,0 +1,35 @@
+package mist.adventofcode.day2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main01 {
+
+  public static void main(String[] args) throws IOException{
+    InputStream r = Main01.class.getClassLoader().getResourceAsStream("day2/input01.txt");
+    Scanner scn = new Scanner(r);
+    
+    int depth = 0;
+    int pos = 0;
+    
+    while(scn.hasNextLine()){
+      String line  = scn.nextLine();
+      String[] split = line.split(" ");
+      int value = Integer.parseInt(split[1]);
+      if(split[0].equals("forward")){
+        pos += value;
+      }else if (split[0].equals("down")){
+        depth += value;
+      }else if (split[0].equals("up")){
+        depth -= value;
+      }else{
+        System.exit(-1);
+      }
+    } 
+    
+    System.out.println("we are at " + pos + " at depth " + depth);
+    System.out.println("multiplied: " + (pos*depth));
+  }
+
+}
---
+++ adventofcode01/src/main/java/mist/adventofcode/day2/Main02.java
@@ -0,0 +1,38 @@
+package mist.adventofcode.day2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main02 {
+
+  public static void main(String[] args) throws IOException{
+    InputStream r = Main02.class.getClassLoader().getResourceAsStream("day2/input01.txt");
+    Scanner scn = new Scanner(r);
+    
+    int depth = 0;
+    int pos = 0;
+    
+    int aim = 0;
+    
+    while(scn.hasNextLine()){
+      String line  = scn.nextLine();
+      String[] split = line.split(" ");
+      int value = Integer.parseInt(split[1]);
+      if(split[0].equals("forward")){
+        pos += value;
+        depth += value*aim;
+      }else if (split[0].equals("down")){
+        aim += value;
+      }else if (split[0].equals("up")){
+        aim -= value;
+      }else{
+        System.exit(-1);
+      }
+    } 
+    
+    System.out.println("we are at " + pos + " at depth " + depth);
+    System.out.println("multiplied: " + (pos*depth));
+  }
+
+}
