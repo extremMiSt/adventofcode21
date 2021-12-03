6d49c47516ef8bcea8922c4bffdcaaf1ef3c7a17
Michael Steinle <michael.steinle@gmail.com>
2021-12-03 11:37:46 +0100

    day 3 done

---
+++ adventofcode01/src/main/java/mist/adventofcode/day3/Main01.java
@@ -0,0 +1,37 @@
+package mist.adventofcode.day3;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main01 {
+
+  public static void main(String[] args) throws IOException{
+    InputStream r = Main01.class.getClassLoader().getResourceAsStream("day3/input01.txt");
+    Scanner scn = new Scanner(r);
+    
+    int count = 0;
+    int[] counts = new int[12];
+    
+    while(scn.hasNextLine()){
+      String line  = scn.nextLine();
+      for (int i = 0; i < counts.length; i++) {
+        if(line.charAt(i) == '1'){
+          counts[i]++;
+        }
+      }
+      count++;
+    }
+    
+    String gammabin = "";
+    String epsilonbin = "";
+    for (int i = 0; i < counts.length; i++) {
+      gammabin = gammabin + (counts[i] < (count-counts[i]) ? "0":"1");
+      epsilonbin = epsilonbin + (counts[i] < (count-counts[i]) ? "1":"0");
+    }
+    System.out.println("gamma: 0b" + gammabin + ", epsilon: 0b" + epsilonbin);
+    System.out.println("gamma: " + Integer.parseInt(gammabin, 2) + ", epsilon: " + Integer.parseInt(epsilonbin, 2));
+    System.out.println("consumption: " + (Integer.parseInt(gammabin, 2) * Integer.parseInt(epsilonbin, 2)));
+  }
+
+}
---
+++ adventofcode01/src/main/java/mist/adventofcode/day3/Main02.java
@@ -0,0 +1,56 @@
+package mist.adventofcode.day3;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main02 {
+
+  public static void main(String[] args) throws IOException{
+    InputStream r = Main02.class.getClassLoader().getResourceAsStream("day3/input01.txt");
+    Scanner scn = new Scanner(r);
+    
+    ArrayList<String> in = new ArrayList<>();
+    while(scn.hasNextLine()){
+      String line  = scn.nextLine();
+      in.add(line);
+    }
+    
+    String gen = find(in, '0', '1');
+    String scrub = find(in, '1', '0');
+    
+    System.out.println("generator: 0b"+ gen + ", scrubber: 0b" + scrub);
+    System.out.println("generator: "+ Integer.parseInt(gen, 2) + ", scrubber: " + Integer.parseInt(scrub, 2));
+    System.out.println("life support: " + Integer.parseInt(gen, 2) * Integer.parseInt(scrub, 2));
+    
+  }
+  
+  public static int count(ArrayList<String> in, int i){
+    int count = 0;
+    for (String num : in) {
+      if(num.charAt(i) == '1'){
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  public static String find(ArrayList<String> in, char a, char b){
+    ArrayList<String> data = new ArrayList<>(in);
+    for (int i = 0; i < 12 && data.size() > 1; i++) {
+      int count = count(data, i);
+      char match = count < data.size()-count ? a:b;
+      
+      ArrayList<String> survivors = new ArrayList<>();
+      for (String string : data) {
+        if(string.charAt(i) == match){
+          survivors.add(string);
+        }
+      }
+      data = survivors;
+    }
+    return data.get(0);
+  }
+
+}
