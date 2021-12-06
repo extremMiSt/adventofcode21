f5a0524a632758fc9453c4bd92b1a4bc3b4d088f
Michael Steinle <michael.steinle@gmail.com>
2021-12-06 10:31:55 +0100

    day 6 done

---
+++ adventofcode01/src/main/java/mist/adventofcode/day6/Main02.java
@@ -0,0 +1,43 @@
+package mist.adventofcode.day6;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main02 {
+  
+  public static void main(String[] args) throws IOException{
+    InputStream r = mist.adventofcode.day3.Main01.class.getClassLoader().getResourceAsStream("day6/input01.txt");
+    Scanner scn = new Scanner(r);
+    String[] squidsRaw = scn.nextLine().split(",");
+    long[] squidsAge = new long[9];
+    for (String string : squidsRaw) {
+      squidsAge[Integer.parseInt(string)]++;
+    }
+    System.out.println("Initial: " + Arrays.toString(squidsAge));
+    
+    for (int i = 1; i <= 256; i++) {
+      //each squid gets a day older
+      long day0s = squidsAge[0];
+      for (int j = 1; j < squidsAge.length; j++) {
+        squidsAge[j-1] = squidsAge[j];
+      }
+      squidsAge[squidsAge.length-1] = 0;
+      
+      //squids that were at 0 get back to 6
+      squidsAge[6] += day0s;
+      //squids that were at 0 get a child at 8
+      squidsAge[8] += day0s;
+      System.out.println("After " + i + " days: " +  Arrays.toString(squidsAge));
+    }
+    
+    System.out.println("Final: " + Arrays.toString(squidsAge));
+    long total = 0;
+    for (int i = 0; i < squidsAge.length; i++) {
+      total += squidsAge[i];
+    }
+    System.out.println("Total: " + total);
+  }
+  
+}
