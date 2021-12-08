d3dae9bde35f9f973239fb02c1d4fbf783bf58c5
Michael Steinle <michael.steinle@gmail.com>
2021-12-08 19:54:20 +0100

    day8 done, cleanup for the 2nd task

--- adventofcode01/src/main/java/mist/adventofcode/day8/Main02.java
+++ adventofcode01/src/main/java/mist/adventofcode/day8/Main02.java
@@ -11,6 +11,13 @@ import static java.util.Map.entry;
 
 public class Main02 {
   
+  private static final Map<Integer, String> MAP = Map.ofEntries(entry(42, "0"), 
+      entry(17, "1"), entry(34, "2"), entry(39, "3"), entry(30, "4"), 
+      entry(37, "5"), entry(41, "6"), entry(25, "7"), entry(49, "8"), 
+      entry(45, "9"));
+
+  private static final char[] SIGNAL = new char[]{'a','b','c','d','e', 'f', 'g'};
+  
   public static void main(String[] args) throws IOException{
     InputStream r = mist.adventofcode.day3.Main01.class.getClassLoader().getResourceAsStream("day8/input01.txt");
     Scanner scn = new Scanner(r);
@@ -18,26 +25,16 @@ public class Main02 {
     List<String> inputs = new ArrayList<>();
     List<String[]> outputs = new ArrayList<>();
     
-    while(scn.hasNextLine()){
-      String line = scn.nextLine();
-      String[] split = line.split("[|]");
-      inputs.add(split[0]);
-      outputs.add(split[1].trim().split("\\s"));
-    }
-    
-    
-    Map<Integer, String> map = Map.ofEntries(entry(42, "0"), entry(17, "1"), 
-        entry(34, "2"), entry(39, "3"), entry(30, "4"), entry(37, "5"), 
-        entry(41, "6"), entry(25, "7"), entry(49, "8"), entry(45, "9"));
-    
-    char[] lines = new char[]{'a','b','c','d','e', 'f', 'g'};
-    
     int sum = 0;
-    for (int i = 0; i < inputs.size(); i++) {
+    while(scn.hasNextLine()){
+      String l = scn.nextLine();
+      String[] split = l.split("[|]");
+      String input = split[0];
+      String[] output = split[1].trim().split("\\s");
+      
       Map<Character, Integer> count = new HashMap<>();
       
-      String input = inputs.get(i);
-      for (char line : lines) {
+      for (char line : SIGNAL) {
         for (int j = 0; j < input.length(); j++) {
           if(line == input.charAt(j)){
             count.put(line, count.getOrDefault(line, 0)+1);
@@ -45,17 +42,17 @@ public class Main02 {
         }
       }
       
-      String[] output = outputs.get(i);
       String out = "";
       for (String string : output) {
         int counter = 0;
         for (int j = 0; j < string.length(); j++) {
           counter += count.get(string.charAt(j));
         }
-        out += map.get(counter);
+        out += MAP.get(counter);
       }
       sum += Integer.parseInt(out);
     }
+    
     System.out.println(sum);
     
   }
